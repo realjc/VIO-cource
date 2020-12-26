6f8232d671e5b1e30230ef179d62cfb66511a7ce
realjc <2904152204@qq.com>
2020-12-26 13:20:22 +0800

    add 作业3 code

---
+++ 刘佳诚_第三章作业/code/part3/script/plotLambda.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Data for plotting
+data = np.loadtxt("/home/null/Documents/VIO-cource/刘佳诚_第三章作业/code/part3/build/lambda_set.txt")
+data = data.T
+fig, ax = plt.subplots()
+ax.plot(data[0], data[1])
+
+ax.set(xlabel='iteration', ylabel='lambda',
+       title='lambda change by iterations')
+ax.grid()
+
+fig.savefig("../../lambda.png")
+plt.show()
